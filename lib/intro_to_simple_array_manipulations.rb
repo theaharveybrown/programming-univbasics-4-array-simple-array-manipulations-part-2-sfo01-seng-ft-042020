dbc060e3527b1459bba1d1a1e1f8e84d0d0d2609
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-06 19:13:04 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array1, array2) 
+  array1.concat(array2)
+end 
+
+def using_insert(array, element)
+  array.insert(4, element)
+end 
+
+def using_uniq(array)
+  array.uniq
+end 
+
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array, str)
+  array.delete(str)
+end 
+
+def using_delete_at(array, int)
+  array.delete_at(int)
+end 
